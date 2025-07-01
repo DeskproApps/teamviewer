4abec204a839dd078ea77a34bfcae580beafaef6
OJ Abba <phavorabba@gmail.com>
2025-07-01 15:39:13 +0100

    [Chore] Introduce errors 💥

--- src/pages/LoadingAppPage/LoadingAppPage.tsx
+++ src/pages/LoadingAppPage/LoadingAppPage.tsx
@@ -1,11 +1,38 @@
 import { LoadingSpinner } from "@deskpro/app-sdk";
-import { useCheckIsAuth } from "./hooks";
+// import { useCheckIsAuth } from "./hooks";
+import { useState, useEffect } from "react";
+import { Button } from "@deskpro/deskpro-ui";
 
 const LoadingAppPage = () => {
-    useCheckIsAuth();
+    // useCheckIsAuth();
+
+    const [errorState, setErrorState] = useState<string | null>(null)
+
+    useEffect(() => {
+        if (errorState === "left") {
+            throw new Error("Hello from TeamViewer")
+        }
+
+        if (errorState === "right") {
+            throw "HI from TeamViewer"
+        }
+
+
+
+    }, [errorState])
 
     return (
-        <LoadingSpinner/>
+        <>
+            <Button
+                text="Left Error"
+                onClick={() => { setErrorState("left") }}
+            />
+            <Button
+                text="Right Error"
+                onClick={() => { setErrorState("right") }}
+            />
+            <LoadingSpinner />
+        </>
     );
 }
 
